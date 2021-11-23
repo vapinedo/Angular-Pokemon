ca416541cc6ab60dce6721c66fd225fa85646939
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-23 16:34:34 -0500

    Fix alias path & register Core module inside app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
+const components = [
+  AppComponent
+];
+
+const modules = [
+  BrowserModule,
+  AppRoutingModule,
+  CoreModule,
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [components],
+  imports: [modules],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
