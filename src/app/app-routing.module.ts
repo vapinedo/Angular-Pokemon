e0ad9ec539683c5a464424326e3916c629918aca
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-23 17:06:00 -0500

    App Routing module paths created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pokemon',
+    loadChildren: () => import('@feature/pokemon/pokemon.module')
+      .then(m => m.PokemonModule)
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
