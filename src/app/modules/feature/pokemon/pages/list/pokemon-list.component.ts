1926a664f1cb82d179a7c59f0123ef6c8f4da58b
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-25 11:11:29 -0500

    GoBack button added to pokemon-list-component

--- src/app/modules/feature/pokemon/pages/list/pokemon-list.component.ts
+++ src/app/modules/feature/pokemon/pages/list/pokemon-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { PokemonService } from '@core/services/pokemon.service';
 import { PokemonShort } from '@core/interfaces/pokemon.interface';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -9,21 +10,35 @@ import { PokemonShort } from '@core/interfaces/pokemon.interface';
 })
 export class PokemonListComponent implements OnInit {
 
+  scrollAmount = 300;
+  isVisibleBackTopBtn: boolean = false;
   public pokemonList: PokemonShort[] = [];
-
+  
   constructor( 
-    private pokemonSvc: PokemonService
+    private pokemonSvc: PokemonService,
+    @Inject(DOCUMENT) private document: Document
   ) {}
-
-  ngOnInit(): void {
-    this.getPokemonList();
+    
+    ngOnInit(): void {
+      this.getPokemonList();
+    }
+    
+    async getPokemonList() {
+      const pokemonList = await this.pokemonSvc.getPokemonList();
+      if (pokemonList && pokemonList.length > 0) {
+        this.pokemonList = pokemonList;
+      }
+    }
+    
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    const yOffset = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    this.isVisibleBackTopBtn = (yOffset || scrollTop) > this.scrollAmount;
   }
 
-  async getPokemonList() {
-    const pokemonList = await this.pokemonSvc.getPokemonList();
-    if (pokemonList && pokemonList.length > 0) {
-      this.pokemonList = pokemonList;
-    }
+  onBackTop() {
+    this.document.documentElement.scrollTop = 0;
   }
   
 }
