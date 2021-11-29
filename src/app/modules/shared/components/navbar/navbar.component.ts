23d6c6e253b11ce87f0f060a61bb25cda1efd853
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-11-29 15:04:41 -0500

    navbar comp changes

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  appName: string = "Pokemon";
   logo: string = "../../../../../assets/img/logo.png";
 
   constructor(
